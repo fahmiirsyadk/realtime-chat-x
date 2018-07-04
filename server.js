c1b20519419f6a44eca380f7a378b4add2d98b55
blubMe <fahmiirsyad10@gmail.com>
2018-07-05 06:59:51 +0700

    configure socket io and detec host

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const app = require('express')()
 const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world!</h1>')
+  res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', socket => {
+  console.log('a user connected')
 })
 
 server.listen(4000, () => {
