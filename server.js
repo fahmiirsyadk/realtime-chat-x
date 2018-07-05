bae49d52e149b3693049f7beeb1d65e3b28d0a6a
blubMe <fahmiirsyad10@gmail.com>
2018-07-05 07:07:49 +0700

    chat send broadcast terminal test

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.get('/', (req, res) => {
 
 io.on('connection', socket => {
   console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg)
+  })
 })
 
 server.listen(4000, () => {
