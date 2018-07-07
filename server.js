f82da3aca76e5cfe793c1c66165b465e547b8f48
blubMe <fahmiirsyad10@gmail.com>
2018-07-08 05:39:02 +0700

    Update v0.0.3 Debug mode
    - add feature to detec client is join / leave the server
    - add feedback client typing
    - some improvement

--- server.js
+++ server.js
@@ -5,27 +5,47 @@ const io = require('socket.io')(server)
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
-
+var numClient = 0
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  // default username , why im using date ?
-  // is tricky dude i want unique id every connection created
+  var addedClient = false
   var currentdate = new Date()
   socket.username = 'Anonymous ' + currentdate.getDay() + currentdate.getMilliseconds()
-  socket.on('disconnect', () => {
-    io.emit('disconnect')
-    console.log('user disconnected')
+  numClient++
+  socket.emit('get_data', {
+    numClient: numClient
   })
+    // BC globaly that some Boiiii connected to our server
+    socket.broadcast.emit('user_joined',{
+      username : socket.username,
+      numClient: numClient
+    })
+
+  // chat behavior to self
   socket.on('chat_message', (data) => {
-    io.sockets.emit('chat_message', {message: data.message, username: socket.username})
+    io.sockets.emit('chat_message', {
+      message: data.message,
+      username: socket.username
+    })
   })
-  socket.on('typing',(data) =>{
-    socket.broadcast.emit('typing', {username: socket.username})
+  // typing behavior
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', {
+      username: socket.username
+    })
+  })
+  // disconnect info
+  socket.on('disconnect', () => {
+    numClient--
+    socket.broadcast.emit('user_left',{
+      username: socket.username,
+      numClient: numClient
+    })
   })
 })
+
 io.emit('some event', {
-    for: 'everyone'
-  })
+  for: 'everyone'
+})
 
 server.listen(4000, () => {
   console.log('The server is running: http://localhost:4000')
